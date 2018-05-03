892d2e2b8588cda34e60522a496d5cc53dd04e08
akshayuppal3 <akshayuppal3@gmail.com>
2018-05-03 02:55:41 -0500

    new function prediction

--- util.py
+++ util.py
@@ -32,111 +32,121 @@ import warnings
 def warn(*args, **kwargs):
     pass
 
+#testing function
+#to do create a function that would do the prediction
+def prediction_models(models,X,Y):
+    for name,model in models:
+        Y_pred = model.predict(X)
+        score = accuracy_score(Y,Y_pred)
+        print("accuarcy ",name,":", score)
+
+def prediction(model,X,y):
+    Y_pred = model.predict(X)
+    score = accuracy_score(y,Y_pred)
+    return(score)
+
 #@return accuarcy of the model
-def dt_best_features(X_train,Y_train,X_test, Y_test):
+def dt_best_features(X_train,Y_train,X_dev, Y_dev):
     clf = ExtraTreesClassifier()
     clf = clf.fit(X_train, Y_train)
     model = SelectFromModel(clf, prefit=True)
     X_new = model.transform(X_train)
-    X_test = model.transform(X_test)
+    X_dev = model.transform(X_dev)
     # baseline_models(X_new,Y_train,X_dev_new,Y_dev)
-    Y_pred,score,_ = decision_tree(X_train,Y_train,X_test,Y_test)
+    Y_pred,score,_ = decision_tree(X_train,Y_train,X_dev,Y_dev)
     return(score)
 
 #baseline models
-def perceptron(X_train,Y_train,X_test,Y_test):
+def perceptron(X_train,Y_train):
     clf = Perceptron(tol= None,random_state = 4)
     clf.fit(X_train,Y_train)
-    Y_pred = clf.predict(X_test)
-    score = accuracy_score(Y_test,Y_pred)
-    return(Y_pred,score,clf)
+    # Y_pred = clf.predict(X_dev)
+    # score = accuracy_score(Y_dev,Y_pred)
+    return(clf)
 
-def decision_tree(X_train,Y_train,X_test,Y_test):
+def decision_tree(X_train,Y_train):
     param_grid = [{'max_depth':np.arange(2,50)}]
     tree = GridSearchCV(DecisionTreeClassifier(),param_grid) 
     tree.fit(X_train,Y_train)
-    Y_pred = tree.predict(X_test)
-    score = accuracy_score(Y_test,Y_pred)
+    # Y_pred = tree.predict(X_dev)
+    # score = accuracy_score(Y_dev,Y_pred)
     #print("Accuracy : decision_tree: ", score)
-    #score = classification(Y_test,Y_pred)
-    return(Y_pred,score,dt)
+    #score = classification(Y_dev,Y_pred)
+    return(dt)
 
-def baseline_models(X_train,Y_train,X_test,Y_test):
-    print("Running the baseline models")    
-    Y_pred_perc,perc_score = perceptron(X_train,Y_train,X_test,Y_test)
-    print("Accuracy : perceptron: ", perc_score)
-    classification(Y_test,Y_pred_perc)
-    
+def baseline_models(X_train,Y_train):
+    models = {}
+    print("Training the baseline models")    
+    perc = perceptron(X_train,Y_train)
+    #print("Development Accuracy : perceptron: ", perc_score)
+    knn = knn(X_train,Y_train)
+    dt = decision_tree(X_train,Y_train)
+    models = {"perceptron":perc,"knn":knn,"decision_tree":dt}
+    return(models)
 
-def classification(Y_test,Y_pred):
-    confusion_matrix(Y_test,Y_pred)
-    print('  Classification Report:\n',classification_report(Y_test,Y_pred),'\n')
+def classification(Y_dev,Y_pred):
+    confusion_matrix(Y_dev,Y_pred)
+    print('  Classification Report:\n',classification_report(Y_dev,Y_pred),'\n')
 
 #better models
-def svm_wrapper(X_train,Y_train,X_test,Y_test):
+def svm_wrapper(X_train,Y_train):
     param_grid = [
     {'C': [1, 10, 100, 1000], 'kernel': ['linear']},
     {'C': [1, 10, 100, 1000], 'gamma': [1,0.1,0.01,0.001, 0.0001], 'kernel': ['rbf']},]
     clf = GridSearchCV(SVC(),param_grid)
     clf.fit(X_train, Y_train)
-    Y_pred = clf.predict(X_test)
-    score = accuracy_score(Y_test,Y_pred)
-    return(score,Y_pred,clf)
+    return(clf)
 
-def voting_classifier(X_train,Y_train,X_test,Y_test):
-    _,_,dt = decision_tree(X_train,Y_train,X_test,Y_test)
-    _,_,perc = perceptron(X_train,Y_train,X_test,Y_test)
-    _,_,svm = svm_wrapper(X_train,Y_train,X_test,Y_test)
+def voting_classifier(X_train,Y_train,X_dev,Y_dev):
+    dt = decision_tree(X_train,Y_train,X_dev,Y_dev)
+    perc = perceptron(X_train,Y_train,X_dev,Y_dev)
+    svm = svm_wrapper(X_train,Y_train,X_dev,Y_dev)
     eclf = VotingClassifier(estimators=[('dt', dt),('svc', svm),('perc',perc)], voting='hard', weights=[1,3,3]) #('svc', svm),('knn', knn)
     eclf.fit(X_train, Y_train)
-    Y_pred = eclf.predict(X_test)
-    score1 = accuracy_score(Y_test,Y_pred)
+    Y_pred = eclf.predict(X_dev)
+    score1 = accuracy_score(Y_dev,Y_pred)
     return(score1)
 
 #Changed the logic with grid search
-def mlp_wrapper(X_train,Y_train, X_test,Y_test):
+def mlp_wrapper(X_train,Y_train):
     param_grid = [{'hidden_layer_sizes':np.arange(10,150,10),'max_iter':[30] ,'activation':['logistic', 'tanh','relu']}]
     mlp = GridSearchCV(MLPClassifier(), param_grid)
     mlp.fit(X_train, Y_train)
-    Y_pred = mlp.predict(X_test)
-    score = accuracy_score(Y_test,Y_pred)
-    return(score,Y_pred)
+    return(mlp)
 
 #from sklearn.preprocessing import CategoricalEncoder
-def knn(X_train,Y_train, X_test,Y_test):
+def knn(X_train,Y_train):
     param_grid = [{'n_neighbors': np.arange(2,250,20)}]
     knn = GridSearchCV(KNeighborsClassifier(),param_grid) 
     knn.fit(X_train,Y_train)
-    Y_pred = knn.predict(X_test)
-    score = (np.mean(Y_pred == Y_test))
-    return(score,Y_pred)
+    return(knn)
 
-def naive_bayes(X_train,Y_train, X_test, Y_test):
+def naive_bayes(X_train,Y_train):
     clf = BernoulliNB()
     clf.fit(X_train, Y_train)
-    Y_pred = neigh.predict(X_test)
-    score = accuracy_score(Y_test,Y_pred)
-    return(score,Y_pred)
+    return(clf)
 
-def extended_model(X_train,Y_train,X_test,Y_test):
-    score,Ypred =knn(X_train,Y_train, X_test,Y_test)
+#to do change
+def extended_model(X_train,Y_train,X_dev,Y_dev):
+    score,Ypred =knn(X_train,Y_train, X_dev,Y_dev)
     print("Accuracy : knn ", score)
-    score, Y_pred = mlp_wrapper(X_train, Y_train, X_test, Y_test, 1500, 'relu')
+    score, Y_pred = mlp_wrapper(X_train, Y_train, X_dev, Y_dev, 1500, 'relu')
     print("Accuracy : MLP ", score)
-    score, Y_pred = svm_wrapper(X_train,Y_train,X_test,Y_test)
+    score, Y_pred = svm_wrapper(X_train,Y_train,X_dev,Y_dev)
     print("Accuracy: SVM ", score)
-    score, Y_pred = bagging(X_train,Y_train, X_test, Y_test)
+    score, Y_pred = bagging(X_train,Y_train, X_dev, Y_dev)
     print("Accuracy: bagging:  ", score)
 #v1
-def bagging_with_tree(X_train,Y_train,X_test,Y_test,depth):
-    _,_,dt =decision_tree(X_train,Y_train,X_test,Y_test,depth)
-    BaggingClassifier(n_estimators = n_estimator,random_state= 4,base_estimator = dt)
-    bag.fit(X_train, Y_train)
-    Y_pred = bag.predict(X_test)
-    score = accuracy_score(Y_test,Y_pred)
-    return(score)       
-#v2: Current version
-def bagging_with_DT(X_train,Y_train, X_test, Y_test):
+# def bagging_with_tree(X_train,Y_train,X_dev,Y_dev,depth):
+#     dt =decision_tree(X_train,Y_train)
+#     BaggingClassifier(n_estimators = n_estimator,random_state= 4,base_estimator = dt)
+#     bag.fit(X_train, Y_train)
+#     Y_pred = bag.predict(X_dev)
+#     score = accuracy_score(Y_dev,Y_pred)
+#     return(score)   
+
+#v2: Current version @@prints
+def bagging_with_DT(X_train,Y_train, X_dev, Y_dev):
     for i in range(1,5):
         dt = DecisionTreeClassifier(max_depth = i,random_state = 4)
         param_grid = [{'n_estimators': np.arange(1,30),'base_estimator':[dt]}]
@@ -148,14 +158,13 @@ def bagging_with_DT(X_train,Y_train, X_test, Y_test):
     print("bagging with decision tree:", sc)
     # return(score,Y_pred)        
 
-def boosting(X_train,Y_train, X_test, Y_test):
+def boosting(X_train,Y_train):
     gb= GradientBoostingClassifier()
     gb.fit(X_train, Y_train)
-    Y_pred = bag.predict(X_test)
-    score = np.mean(gb.predict(X_dev) == Y_dev)
-    return(score,Y_pred)
+    return(gb)
 
-def PCA_with_DT(X_train,Y_train, X_test, Y_test):
+##@@print
+def PCA_with_DT(X_train,Y_train):
     score = [0]
     for i in range(1,20):
         estimators1 = [('tree', PCA()), ('clf', DecisionTreeClassifier(max_depth =i,random_state = 4) )]
@@ -166,10 +175,9 @@ def PCA_with_DT(X_train,Y_train, X_test, Y_test):
     sc = np.sort(score)[::-1][0]
     print("PCA with DT:", sc )
 
-def PCA_with_SVM(X_train,Y_train, X_test, Y_test):
+##@returns PCA_with_svm model
+def PCA_with_SVM(X_train,Y_train):
     estimators = [('reduce_dim', PCA()), ('clf', SVC() )]
     pipe = Pipeline(estimators)
     pipe.fit(X_train,Y_train)
-    Y_pred = pipe.predict(X_dev)
-    score = (np.mean(Y_pred == Y_dev))
-    print(score)
+    return(pipe)
